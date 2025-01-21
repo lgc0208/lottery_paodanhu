218bb7c8c1d2595996eb2272e2a621d3de8d7855
lgc0208 <lgc0208@foxmail.com>
2025-01-21 20:25:08 +0800

    main: 更新 新抽奖名单和抽奖规则

--- js/data/data-first.js
+++ js/data/data-first.js
@@ -1,14 +1,10 @@
 define([
-    {"id":"10","company":"五一馆","name":"高晓凤"},
-    {"id":"12","company":"国泰馆","name":"何婷"},
-    {"id":"14","company":"国泰馆","name":"程玉磊"},
-    {"id":"15","company":"紫阳馆","name":"范馨婷"},
-    {"id":"16","company":"紫阳馆","name":"朱元津"},
-    {"id":"18","company":"侨雄馆","name":"孙启明"},
-    {"id":"19","company":"鸿雁馆","name":"黄震"},
-    {"id":"21","company":"马尾馆","name":"王代灿"},
-    {"id":"26","company":"侨雄馆","name":"王烨珍"},
-    {"id":"28","company":"侨雄馆","name":"关志铭"},
-    {"id":"29","company":"屏东馆","name":"柯秀金"},
-    {"id":"30","company":"屏东馆","name":"王鑫"},
-])
+    {"id": "1", "company": "", "name": "程玉磊"},
+    {"id": "2", "company": "", "name": "郑永健"},
+    {"id": "3", "company": "", "name": "何婷"},
+    {"id": "5", "company": "", "name": "吴浩轩"},
+    {"id": "11", "company": "", "name": "范范"},
+    {"id": "12", "company": "", "name": "柯秀金"},
+    {"id": "14", "company": "", "name": "程俊"},
+    {"id": "15", "company": "", "name": "王代灿"}
+  ])
